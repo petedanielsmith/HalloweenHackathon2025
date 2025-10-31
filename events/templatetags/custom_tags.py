cced1795ab14e4a2e570ba3949a348d051014b6f
Pete <pete@petedanielsmith.co.uk>
2025-10-31 09:02:27 +0000

    Fix bug on contact form centuries dropdown

--- events/templatetags/custom_tags.py
+++ events/templatetags/custom_tags.py
@@ -12,3 +12,19 @@ def dict_get(d, key):
         except Exception:
             return 0
     return 0
+
+@register.filter
+def split(value, sep=","):
+    return [s for s in value.split(sep) if s]
+
+@register.filter
+def ordinal(value):
+    try:
+        n = int(value)
+    except (TypeError, ValueError):
+        return value
+    if 10 <= n % 100 <= 20:
+        suffix = "th"
+    else:
+        suffix = {1: "st", 2: "nd", 3: "rd"}.get(n % 10, "th")
+    return f"{n}{suffix}"
